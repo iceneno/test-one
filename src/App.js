eb1dc7be5e37759c9dbf01e4f2a6db4caab64d86
Ice Neno <iceneno@gmail.com>
2021-02-24 12:47:53 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Moja odlična react aplikacija...
+          Moja odlična react aplikacija 2...
         </p>
 
       </header>
